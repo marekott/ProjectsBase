36a12e3fc293545ab8eb3e2ca2aa8d9514dd3465
Marek Ott <marekott94@gmail.com>
2019-08-01 20:32:12 +0200

    Test refactor

--- src/ProjectsBaseShared/ProjectsBaseSharedTests/Data/ProjectsRepositoryTests.cs
+++ src/ProjectsBaseShared/ProjectsBaseSharedTests/Data/ProjectsRepositoryTests.cs
@@ -26,6 +26,9 @@ namespace ProjectsBaseSharedTests.Data
             _project = InitProject();
 
             _projectsRepository.Add(_project);
+
+            Assert.True(_project.ProjectId != Guid.Empty, "Add method returned empty guid.");
+
             _projectId = _project.ProjectId;
         }
 
@@ -39,18 +42,23 @@ namespace ProjectsBaseSharedTests.Data
             };
         }
 
-        [TearDown]
-        public void Remove()
-        {
-            _projectsRepository.Delete(_project);
-        }
+        //[TearDown]
+        //public void Remove()
+        //{
+        //    _projectsRepository.Delete(_project);
+        //}
 
         [Test]
         public void GetOnlyProjectTest()
         {
             var downloadedProject = _projectsRepository.Get(_projectId, false);
 
-            Assert.True(downloadedProject.Equals(_project));
+            Assert.True(downloadedProject.Equals(_project), "GetOnlyProjectTest returned wrong id");
+            Assert.AreEqual(ProjectName, downloadedProject.ProjectName, "GetOnlyProjectTest returned wrong project name.");
+            Assert.AreEqual(_projectStartDate, downloadedProject.ProjectStartDate , "GetOnlyProjectTest returned wrong start date.");
+            Assert.AreEqual(_projectEndDate, downloadedProject.ProjectEndDate, "GetOnlyProjectTest returned wrong end date.");
+            Assert.True(downloadedProject.Auditors.Count == 0, "GetOnlyProjectTest returned related auditors.");
+            Assert.IsNull(downloadedProject.Client, "GetOnlyProjectTest returned related client.");
         }
 
         [Test]
@@ -61,38 +69,29 @@ namespace ProjectsBaseSharedTests.Data
             Assert.IsNull(downloadedProject);
         }
 
-        [Test]
-        public void GetProjectAndRelatedTest()
-        {
-            //TODO
-            throw new NotImplementedException();
-        }
+        //[Test]
+        //public void GetProjectAndRelatedTest()
+        //{
+        //    //TODO
+        //    throw new NotImplementedException();
+        //}
 
-        [Test]
-        public void GetProjectAndRelatedWhichNotExistTest()
-        {
-            //TODO
-            throw new NotImplementedException();
-        }
-
-        [Test]
-        public void AddTest() //add method is called during SetUp so if _project exists in the database the test has passed. 
-        {
-            GetOnlyProjectTest();
-        }
+        //[Test]
+        //public void GetProjectAndRelatedWhichNotExistTest()
+        //{
+        //    //TODO
+        //    throw new NotImplementedException();
+        //}
 
         [Test]
         public void DeleteTest()
         {
-            var project = InitProject();
+            var downloadedProject = _projectsRepository.Get(_projectId, false);
+            Assert.IsNotNull(downloadedProject, "Project is null before delete.");
 
-            _projectsRepository.Add(project);
-            var downloadedProject = _projectsRepository.Get(project.ProjectId, false);
-            Assert.True(downloadedProject != null);
-
-            _projectsRepository.Delete(project);
-            downloadedProject = _projectsRepository.Get(project.ProjectId, false);
-            Assert.True(downloadedProject == null);
+            _projectsRepository.Delete(_project);
+            downloadedProject = _projectsRepository.Get(_projectId, false);
+            Assert.IsNull(downloadedProject, "Project is not null after delete.");
         }
     }
 }
---
+++ src/ProjectsBaseShared/ProjectsBaseSharedTests/Mock/DatabaseInitializerMock.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+using ProjectsBaseShared.Data;
+
+namespace ProjectsBaseSharedTests.Mock
+{
+    internal class DatabaseInitializerMock : DropCreateDatabaseAlways<Context>
+    {
+    }
+}
