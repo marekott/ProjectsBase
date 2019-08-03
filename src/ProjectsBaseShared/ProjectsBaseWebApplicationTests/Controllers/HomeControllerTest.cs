779fe7bd3d59784ff0d62c6ef6f5494af965984a
Marek Ott <marekott94@gmail.com>
2019-08-03 12:50:04 +0200

    Remove default controllers and views

--- src/ProjectsBaseShared/ProjectsBaseWebApplicationTests/Controllers/HomeControllerTest.cs
+++ src/ProjectsBaseShared/ProjectsBaseWebApplicationTests/Controllers/HomeControllerTest.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectsBaseWebApplication;
+﻿using System.Web.Mvc;
+using NUnit.Framework;
 using ProjectsBaseWebApplication.Controllers;
+using Assert = NUnit.Framework.Assert;
+
 
 namespace ProjectsBaseWebApplicationTests.Controllers
 {
-    [TestClass]
+    [TestFixture]
     public class HomeControllerTest
     {
-        [TestMethod]
+        [Test]
         public void Index()
         {
             // Arrange
@@ -20,32 +17,6 @@ namespace ProjectsBaseWebApplicationTests.Controllers
 
             // Act
             ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
