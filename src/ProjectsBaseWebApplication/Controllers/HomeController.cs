0de0c6fe01aaa450a18d88d7f79b8cff165edc41
Marek Ott <marekott94@gmail.com>
2019-08-03 15:19:29 +0200

    Make Index.cshtml strongly typed and add projects repository to HomdeController.

--- src/ProjectsBaseWebApplication/Controllers/HomeController.cs
+++ src/ProjectsBaseWebApplication/Controllers/HomeController.cs
@@ -1,12 +1,41 @@
 ﻿using System.Web.Mvc;
+using ProjectsBaseShared.Data;
 
 namespace ProjectsBaseWebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Context _context;
+        private readonly ProjectsRepository _projectsRepository;
+        private bool _disposed;
+
+        public HomeController()
+        {
+            _context = new Context();
+            _projectsRepository = new ProjectsRepository(_context);
+        }
         public ActionResult Index()
         {
-            return View();
+            var projects = _projectsRepository.GetList();
+
+            return View(projects);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+
+            _disposed = true;
+
+            base.Dispose(disposing);
         }
     }
 }
