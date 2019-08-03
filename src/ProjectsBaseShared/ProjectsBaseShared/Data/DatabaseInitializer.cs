8281d7265c4ef46361717bb87501fa2cea266d71
Marek Ott <marekott94@gmail.com>
2019-08-03 15:32:00 +0200

    Fix in Web.config and add Seed implementation to database initializer

--- src/ProjectsBaseShared/ProjectsBaseShared/Data/DatabaseInitializer.cs
+++ src/ProjectsBaseShared/ProjectsBaseShared/Data/DatabaseInitializer.cs
@@ -1,8 +1,56 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using ProjectsBaseShared.Models;
 
 namespace ProjectsBaseShared.Data
 {
     internal class DatabaseInitializer : DropCreateDatabaseIfModelChanges<Context>
     {
+        protected override void Seed(Context context)
+        {
+            var clientPzu = new Client()
+            {
+                ClientName = "PZU"
+            };
+
+            var clientPko = new Client()
+            {
+                ClientName = "Pko"
+            };
+
+            var clientIng = new Client()
+            {
+                ClientName = "Ing"
+            };
+
+            var projectPzu = new Project()
+            {
+                ProjectName = "PZU - Audit",
+                ProjectStartDate = DateTime.Now,
+                ProjectEndDate = DateTime.Now.AddMonths(2),
+                Client = clientPzu
+            };
+            context.Projects.Add(projectPzu);
+
+            var projectPko = new Project()
+            {
+                ProjectName = "Pko - Audit",
+                ProjectStartDate = DateTime.Now,
+                ProjectEndDate = DateTime.Now.AddMonths(2),
+                Client = clientPko
+            };
+            context.Projects.Add(projectPko);
+
+            var projectIng = new Project()
+            {
+                ProjectName = "Ing - Audit",
+                ProjectStartDate = DateTime.Now,
+                ProjectEndDate = DateTime.Now.AddMonths(2),
+                Client = clientIng
+            };
+            context.Projects.Add(projectIng);
+
+            context.SaveChanges();
+        }
     }
 }
